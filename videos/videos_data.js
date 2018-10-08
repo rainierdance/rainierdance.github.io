ccb5b5425b3b7e494a9774d27e331797df4e4228
rainierdance <info@rainierdance.com>
2018-10-08 17:16:54 +0100

    update youtube video player urls

--- videos/videos_data.js
+++ videos/videos_data.js
@@ -4,13 +4,13 @@ danceVideos = danceVideos.concat([
     'title' : 'Standard Technique: Take the frame with you',
     'description' : 'Demonstrated Glen Weiss',
     'labels' : [],
-    'url' : 'http://www.youtube.com/v/anAhk5Zw-VA'
+    'url' : 'http://www.youtube.com/watch?v=anAhk5Zw-VA'
   },
   {
     'title' : 'Latin Bondy Roll',
     'description' : 'Demonstrated Monika',
     'labels' : [],
-    'url' : 'http://www.youtube.com/v/KbqHP5qIt0I'
+    'url' : 'http://www.youtube.com/watch?v=KbqHP5qIt0I'
   },
   {
     'title' : 'Standard Technique by Glen Weiss',
@@ -22,13 +22,13 @@ danceVideos = danceVideos.concat([
       '<li> Flex our knees slightly forward  ' +
       '</ul>',
     'labels' : [],
-    'url' : 'http://www.youtube.com/v/kW-ffl7i5-c'
+    'url' : 'http://www.youtube.com/watch?v=kW-ffl7i5-c'
   },
   {
     'title' : 'Latin Styling Demonstration - Portland Dancesport',
     'description' : 'Demonstrated using Cha Cha. 5 points on leg action, body rhythm, arm styling, ronde, swivels.',
     'labels' : [],
-    'url' : 'http://www.youtube.com/v/w98G0tPWTPQ'
+    'url' : 'http://www.youtube.com/watch?v=w98G0tPWTPQ'
   }
 ]);
 
@@ -37,13 +37,13 @@ danceVideos = danceVideos.concat([
     'title' : 'Cha Cha: Natural Top, Closed Hip Twist',
     'description' : 'Instruction by Andy & Wendy Wong.',
     'labels' : ['NaturalTop', 'ClosedHipTwist', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/xqatK2FmMeQ'
+    'url' : 'http://www.youtube.com/watch?v=xqatK2FmMeQ'
   },
   {
     'title' : 'Cha Cha: Fan Development, Hockey Stick',
     'description' : 'Instruction by Andy & Wendy Wong.',
     'labels' : ['HockeyStick', 'Fan', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/zgXV5J1gebA'
+    'url' : 'http://www.youtube.com/watch?v=zgXV5J1gebA'
   },
   {
     'title' : 'Cha Cha: Bronze Practice Routine (Slow Motion)',
@@ -60,7 +60,7 @@ danceVideos = danceVideos.concat([
       '<li>  Time Step ' +
       '</ul>',
     'labels' : ['OpenBasic', 'OpenHipTwist', 'Alemana', 'NewYork', 'NaturalTop', 'TimeStep', 'HockeyStick', 'ClosedHipTwist', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/156NISHQSwQ'
+    'url' : 'http://www.youtube.com/watch?v=156NISHQSwQ'
   },
   {
     'title' : 'Cha Cha: Bronze Practice Routine',
@@ -82,7 +82,7 @@ danceVideos = danceVideos.concat([
       '<li>  Time Step ' +
       '</ul>',
     'labels' : ['Fan', 'SpotTurns', 'ThreeChaChaChas', 'Alemana', 'NewYork', 'HandToHand', 'TimeStep', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/IB5HKa60Ys8'
+    'url' : 'http://www.youtube.com/watch?v=IB5HKa60Ys8'
   },
   {
     'title' : 'Cha Cha: Newcomer Practice Rountine (Slow motion)',
@@ -99,127 +99,127 @@ danceVideos = danceVideos.concat([
       '<li>  Three Cha Cha Chas ' +
       '</ul>',
     'labels' : ['SpotTurns', 'ShoulderToShoulder', 'NewYork', 'CloseDBasic', 'TimeStep', 'UnderarmTurn', 'HandToHand', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v//kjUa_18T9WM'
+    'url' : 'http://www.youtube.com/watch?v=/kjUa_18T9WM'
   },
   {
     'title' : 'Cha Cha: Hand To Hand, Spot Turn',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['HandToHand', 'SpotTurn', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/LarAIW4WtRY'
+    'url' : 'http://www.youtube.com/watch?v=LarAIW4WtRY'
   },
   {
     'title' : 'Cha Cha: Underarm Turn to Left and Right',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['UnderarmTurn', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/0fitf0eXwBI'
+    'url' : 'http://www.youtube.com/watch?v=0fitf0eXwBI'
   },
   {
     'title' : 'Cha Cha: Spot Turn',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['SpotTurn', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/QI_8CcEP-r4'
+    'url' : 'http://www.youtube.com/watch?v=QI_8CcEP-r4'
   },
   {
     'title' : 'Cha Cha: Closed Basic, New York',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['ClosedBasic', 'NewYork', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/2kYm40Ds6pQ'
+    'url' : 'http://www.youtube.com/watch?v=2kYm40Ds6pQ'
   },
   {
     'title' : 'Cha Cha: Fan, Alemana, Time Step',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['TimeStep', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/tzHx05TGEC8'
+    'url' : 'http://www.youtube.com/watch?v=tzHx05TGEC8'
   },
   {
     'title' : 'Cha Cha: Closed Hip Twist - Lady, and with partner',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['ClosedHipTwist', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/03wpaGM0suM'
+    'url' : 'http://www.youtube.com/watch?v=03wpaGM0suM'
   },
   {
     'title' : 'Cha Cha: Closed Hip Twist - Man',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['ClosedHipTwist', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/pyD1q52MGfg'
+    'url' : 'http://www.youtube.com/watch?v=pyD1q52MGfg'
   },
   {
     'title' : 'Cha Cha: Closed Hip Twist - with Partner',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['ClosedHipTwist', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/ICCXzcQZpNY'
+    'url' : 'http://www.youtube.com/watch?v=ICCXzcQZpNY'
   },
   {
     'title' : 'Cha Cha: Overtuned Hockey Stick, Split Cuban Break, Cuban Breaks',
     'description' : 'Instruction by Andy & Wendy Wong.',
     'labels' : ['HockeyStick', 'SplitCubanBreak', 'CubanBreak', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/o4POJq_vZ7U'
+    'url' : 'http://www.youtube.com/watch?v=o4POJq_vZ7U'
   },
   {
     'title' : 'Cha Cha: Time Step',
     'description' : 'Academia Pedro Sousa. (non-English)',
     'labels' : ['TimeStep', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/39nUtVGjTWw'
+    'url' : 'http://www.youtube.com/watch?v=39nUtVGjTWw'
   },
   {
     'title' : 'Cha Cha: Shoulder To Shoulder',
     'description' : '(non-English)  Academia Pedro Sousa.',
     'labels' : ['ShoulderToShoulder', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/8Kt2u6bt3LU'
+    'url' : 'http://www.youtube.com/watch?v=8Kt2u6bt3LU'
   },
   {
     'title' : 'Cha Cha Routine: mini lecture on using spine',
     'description' : 'Demonstrated by Michael & Joanna. Routine uses advanced steps.',
     'labels' : ['ChaCha'],
-    'url' : 'http://www.youtube.com/v/UTJqg3wrNYs'
+    'url' : 'http://www.youtube.com/watch?v=UTJqg3wrNYs'
   },
   {
     'title' : 'Cha Cha Technique: Connection with Floor',
     'description' : 'Demonstrated by Monika.',
     'labels' : ['ChaCha'],
-    'url' : 'http://www.youtube.com/v/3qrYZGI-RKI'
+    'url' : 'http://www.youtube.com/watch?v=3qrYZGI-RKI'
   },
   {
     'title' : 'Cha Cha Basic Movement, Alemana',
     'description' : 'Demonstrated by Monika.',
     'labels' : ['ClosedBasic', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/7JUWUqckohM'
+    'url' : 'http://www.youtube.com/watch?v=7JUWUqckohM'
   },
   {
     'title' : 'Cha Cha Closed Basic',
     'description' : 'Demonstrated by Monika.',
     'labels' : ['ClosedBasic', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/lyAL5Gt21GM'
+    'url' : 'http://www.youtube.com/watch?v=lyAL5Gt21GM'
   },
   {
     'title' : 'Cha Cha Technique: Walks and Locks',
     'description' : 'Demonstrated by Monika.',
     'labels' : ['ThreeChaChaChas', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/hN0vqOVteCI'
+    'url' : 'http://www.youtube.com/watch?v=hN0vqOVteCI'
   },
   {
     'title' : 'Cha Cha Technique: Check, Ronde Chasse, and Twist',
     'description' : 'Demonstrated by Monika.',
     'labels' : ['ChaCha'],
-    'url' : 'http://www.youtube.com/v/8nzaORdW7Gg'
+    'url' : 'http://www.youtube.com/watch?v=8nzaORdW7Gg'
   },
   {
     'title' : 'Cha Cha Technique: Hip movement, Lock Step (forward and backward)',
     'description' : 'Demonstrated by Monika.',
     'labels' : ['ThreeChaChaChas', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/AW7bwJ8SVHw'
+    'url' : 'http://www.youtube.com/watch?v=AW7bwJ8SVHw'
   },
   {
     'title' : 'Cha Cha: New York',
     'description' : 'Demonstrated by Andy & Wendy Wong from grandballroom.com.',
     'labels' : ['NewYorkLSP', 'NewYorkRSP', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/LsU4x_YVvEQ'
+    'url' : 'http://www.youtube.com/watch?v=LsU4x_YVvEQ'
   },
   {
     'title' : 'Cha Cha Rountine - Bronze level',
     'description' : 'A couple in Singapore, preparing for their competition.',
     'labels' : ['ChaCha'],
-    'url' : 'http://www.youtube.com/v/eImhD8_tzBM'
+    'url' : 'http://www.youtube.com/watch?v=eImhD8_tzBM'
   },
   {
     'title' : 'Cha Cha Practice Routine - Bronze Level',
@@ -251,7 +251,7 @@ danceVideos = danceVideos.concat([
       '<li>  Three Cha Cha Chas Forward ' +
       '</ul>',
     'labels' : ['ShoulderToShoulder', 'ChaCha'],
-    'url' : 'http://www.youtube.com/v/RMjgnIMCu5A'
+    'url' : 'http://www.youtube.com/watch?v=RMjgnIMCu5A'
   }
 ]);
 
